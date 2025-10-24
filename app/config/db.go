58d09004711ec1074c116a45119a6843ededeea3
user <msazad202@gmail.com>
2025-10-24 23:46:14 +0530

    added db in config

---
+++ app/config/db.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/joho/godotenv"
+)
+
+func DBConnection() {
+
+	if err:=godotenv.Load();err!=nil{
+		log.Fatalf("loading env faild due to %v",err)
+	}
+
+	user:=os.Getenv("DB_USER")
+	password:=os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	// postgres://postgres:password@localhost:5432/employee?sslmode=disable
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", user, password, host, port, dbname)
+
+	gDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database : %v ", err)
+	}
+
+	if err := AutoMigrateModels(gDB); err != nil {
+		log.Fatalf("automigration err : %v", err)
+	}
+
+	// Geting SQL DB object
+	sqlDB, err := gDB.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Checking the connection is alive
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Configure connection pool
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Connected to Database successfully....")
+
+	return gDB, sqlDB, nil
+
+}
